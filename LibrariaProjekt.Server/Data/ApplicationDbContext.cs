12d1664dcbb8b4abc70a56c8af769e4ebe69852f
Valdet2 <valdet.diti@gmail.com>
2025-11-20 18:47:13 +0100

    konfigurimi tek appsettings.json,edhe DbContext duhet me ndreq edhe pak

---
+++ LibrariaProjekt.Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿/*using System.Collections.Generic;
+using LibrariaProjekt.Server.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibrariaProjekt.Server.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        
+        public DbSet<Book> Books { get; set; }
+    }
+}
+*/
